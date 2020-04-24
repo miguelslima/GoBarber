bbe94f364d7f1236e9a8df3c2e41bb7f38379cd3
Miguel <miguelsousalima@hotmail.com>
2020-04-23 22:27:03 -0300

    Aplicacao inicial back e front

---
+++ backend-GoBarber/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e67e7570f6434fcf1b57224c2306cef7',
+    expiresIn: '1d',
+  },
+};
---
+++ gobarber-web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import AuthContext from './context/AuthContext';
+
+const App: React.FC = () => (
+  <>
+    <AuthContext.Provider value={{ name: 'Miguel' }}>
+      <SignIn />
+    </AuthContext.Provider>
+    <GlobalStyle />
+  </>
+);
+export default App;
