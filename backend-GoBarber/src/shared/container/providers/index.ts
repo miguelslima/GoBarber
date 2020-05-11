ab0145cf1e8195345f2960c6576d667e13c27512
Miguel <miguelsousalima@hotmail.com>
2020-05-10 21:54:56 -0300

    Testes Usuario GoBarber

--- backend-GoBarber/src/shared/container/providers/index.ts
+++ backend-GoBarber/src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStoragerProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+// import IMailProvider from './MailProveider/models/IMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStoragerProvider,
