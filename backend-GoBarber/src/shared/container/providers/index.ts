64afaf3c67102692eee7a65c9f53eb7c57c2584d
Miguel <miguelsousalima@hotmail.com>
2020-05-12 08:56:45 -0300

    Configuracao Mail e recuperacao de senha

--- backend-GoBarber/src/shared/container/providers/index.ts
+++ backend-GoBarber/src/shared/container/providers/index.ts
@@ -3,9 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStoragerProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailProvider from './MailProveider/models/IMailProvider';
+import IMailProvider from './MailProveider/models/IMailProvider';
+import EtherealMailProvider from './MailProveider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStoragerProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
 );
