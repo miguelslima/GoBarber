b3950cf92dfed80a859e9ce63e2235a231e887f8
Miguel <miguelsousalima@hotmail.com>
2020-05-17 23:38:38 -0300

    Config Mongo e Envio de Notificacoes

--- backend-GoBarber/src/modules/appointments/infra/http/routes/appoitments.routes.ts
+++ backend-GoBarber/src/modules/appointments/infra/http/routes/appoitments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appoitmentsRouter = Router();
 const appointmentscController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appoitmentsRouter.use(ensureAuthenticated);
 
 appoitmentsRouter.post('/', appointmentscController.create);
+appoitmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appoitmentsRouter;
