6ab9adc726e30058356da017da9b9a7d8a30ca13
Miguel <miguelsousalima@hotmail.com>
2020-05-22 09:46:01 -0300

    Refatorando testes Cache

--- backend-GoBarber/src/modules/appointments/services/ListProvidersService.spec.ts
+++ backend-GoBarber/src/modules/appointments/services/ListProvidersService.spec.ts
@@ -1,15 +1,21 @@
 // import AppError from '@shared/errors/AppError';
 
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import FakeUserRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import ListProvidersService from './ListProvidersService';
 
 let fakeUserRepository: FakeUserRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let listProviders: ListProvidersService;
 
 describe('ListProviders', () => {
   beforeEach(() => {
     fakeUserRepository = new FakeUserRepository();
-    listProviders = new ListProvidersService(fakeUserRepository);
+    fakeCacheProvider = new FakeCacheProvider();
+    listProviders = new ListProvidersService(
+      fakeUserRepository,
+      fakeCacheProvider,
+    );
   });
 
   it('should be able to list the provider', async () => {
